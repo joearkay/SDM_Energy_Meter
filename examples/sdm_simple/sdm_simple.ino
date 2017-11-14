1010dd9df9a306c90d2198aaf15bc161b7da5136
Joe Richmond-Knight <joe@shprd.com>
2017-11-14 11:32:36 +0000

    updated for example use

--- examples/sdm_simple/sdm_simple.ino
+++ examples/sdm_simple/sdm_simple.ino
@@ -20,21 +20,21 @@ TX_SSer/HSer swap D8|15                            |GND
 
 char bufout[10];
 
-//SDM<2400, 13, 15> sdm;                                                        //SDM120T	baud, rx pin, tx pin, dere pin(optional for max485)
+SDM<2400, 8, 9, 10> sdm;                                                        //SDM120T	baud, rx pin, tx pin, dere pin(optional for max485)
 //SDM<4800, 13, 15> sdm;                                                        //SDM220T	baud, rx pin, tx pin, dere pin(optional for max485)
 //SDM<9600, 13, 15> sdm;                                                        //SDM630	baud, rx pin, tx pin, dere pin(optional for max485)
 //or without parameters (default from SDM.h will be used): 
-SDM<> sdm;
+//SDM<> sdm;
 
 void setup() {
-  Serial.begin(115200);                                                         //initialize serial
+  Serial.begin(9600);                                                         //initialize serial
   sdm.begin();                                                                  //initialize SDM220 communication baudrate
 }
 
 void loop() {
 
-  sprintf(bufout,"%c[1;0H",ASCII_ESC);
-  Serial.print(bufout);
+  //sprintf(bufout,"%c[1;0H",ASCII_ESC);
+  //Serial.print(bufout);
 
   Serial.print("Voltage:   ");
   Serial.print(sdm.readVal(SDM220T_VOLTAGE), 2);                                //display voltage
